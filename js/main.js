b08a8686a23f56a46951d7ec14eeacf3aa692d66
Botond Birton <birtonboti@yahoo.com>
2018-06-13 17:28:46 +0200

    New Html structure and Css Grid, Function Bartender is working

--- js/main.js
+++ js/main.js
@@ -1,20 +1,29 @@
-"use strict"
+"use strict";
 
 let myObject;
+//const tapsTemplate = document.querySelector("#tapsTemplate").content;
 
-document.addEventListener("DOMContentLoaded", loadScript);
 
+window.addEventListener("DOMContentLoaded", loadScript);
+
+//getting the data from the script
 function loadScript() {
     let data = FooBar.getData();
     myObject = JSON.parse(data);
-    console.log(myObject);
 
     //01 Project Name and Opening Hours
-    // Closing Time
     document.querySelector(".project-name").textContent = `${myObject.bar.name}`;
+    // Closing Time
+    document.querySelector(".closing-time").textContent = ` ${myObject.bar.closingTime}`;
     // Current Time
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    document.querySelector(".current-time").textContent = ` ` + hours + ":" + minutes;
 
     //02 Call Bartender's Function
+    //call functions
+    showBartenders();
 
     //03 Number of people served
 
@@ -22,8 +31,59 @@ function loadScript() {
 
     //05. Getting a number of people served now
 
-    // RELOAD all 3 sec for development, for presentation set to 10 sec
-    setInterval(function () {
-        loadScript();
-    }, 2000)
+
+};
+
+
+//02 Bartenders name, status and status detail
+function showBartenders() {
+    //console.log("bartenders", myObject.bartenders);
+    //clean the container of bartenders
+    document.querySelector(".box4").innerHTML = "";
+
+    let bartenders = myObject.bartenders;
+
+    bartenders.forEach(bartender => {
+        //console.log("bartender", bartender.name);
+
+        //define the bartenders template
+        let bartendersTemplate = document.querySelector(".bartendersTemplate").content;
+
+        //define the bartenders clone 
+        let bartendersClone = bartendersTemplate.cloneNode(true);
+        //getting the names of the bartenders
+        bartendersClone.querySelector(".bartender_name").textContent = ` ${bartender.name}`;
+        //bartender´s status of work
+        if (bartender.status == "WORKING") {
+            bartendersClone.querySelector(".bartender_status").textContent = " Working";
+            bartendersClone.querySelector(".bartender_status").style.backgroundColor = "green";
+        } else {
+            bartendersClone.querySelector(".bartender_status").textContent = " Getting Ready";
+            bartendersClone.querySelector(".bartender_status").style.backgroundColor = "orange";
+        }
+
+        //bartender´s precise activity
+        if (bartender.statusDetail == "pourBeer") {
+            bartendersClone.querySelector(".bartender_activity").textContent = ` Pours Beer`;
+        } else if (bartender.statusDetail == "startServing") {
+            bartendersClone.querySelector(".bartender_activity").textContent = ` Starts Serving`;
+        } else if (bartender.statusDetail == "receivePayment") {
+            bartendersClone.querySelector(".bartender_activity").textContent = ` Receives Payment`;
+        } else if (bartender.statusDetail == "releaseTap") {
+            bartendersClone.querySelector(".bartender_activity").textContent = ` Releases Tap`;
+        } else if (bartender.statusDetail == "reserveTap") {
+            bartendersClone.querySelector(".bartender_activity").textContent = ` Reserves Tap`;
+        } else {
+            bartendersClone.querySelector(".bartender_activity").textContent = ` Is waiting`;
+        }
+
+        //append clone in the div .bartenders
+        document.querySelector(".box4").appendChild(bartendersClone);
+    })
+
 }
+
+//setting the interval so the date reloads in 10s !!!IMPORTANT to change the time to 10 s before handin
+setInterval(function () {
+    loadScript();
+}, 3000);
