664310c6550456c8e30cf31b28c4030e6e13de11
Botond Birton <birtonboti@yahoo.com>
2018-06-13 10:37:48 +0200

    Javascript To do list

--- js/main.js
+++ js/main.js
@@ -9,7 +9,16 @@ function loadScript() {
     myObject = JSON.parse(data);
     console.log(myObject);
 
-// RELOAD all 3 sec for development, for presentation set to 10 sec
-setInterval(function () {
-    loadScript();
-}, 2000)}
+    //01 Project Name and Opening Hours
+    // Closing Time
+    // Current Time
+
+    //02 Call Bartender's Function
+
+    //03 Number of people served
+
+    // RELOAD all 3 sec for development, for presentation set to 10 sec
+    setInterval(function () {
+        loadScript();
+    }, 2000)
+}
