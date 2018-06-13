2f8ce863a572b276ec6831928410a750240e9ae9
Botond Birton <birtonboti@yahoo.com>
2018-06-13 10:41:56 +0200

    Project Name is working

--- js/main.js
+++ js/main.js
@@ -11,11 +11,16 @@ function loadScript() {
 
     //01 Project Name and Opening Hours
     // Closing Time
+    document.querySelector(".project-name").textContent = `${myObject.bar.name}`;
     // Current Time
 
     //02 Call Bartender's Function
 
     //03 Number of people served
+
+    //04 Getting a number of beers served in total
+
+    //05. Getting a number of people served now
 
     // RELOAD all 3 sec for development, for presentation set to 10 sec
     setInterval(function () {
