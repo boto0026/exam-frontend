025483eb22362c3238732f56fa665f0083c15d93
Botond Birton <birtonboti@yahoo.com>
2018-06-14 10:09:15 +0200

    Beers Storage Section is working with the grid

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 "use strict";
 
 let myObject;
+let storageArray = [];
+let nameArray = [];
 
 
 window.addEventListener("DOMContentLoaded", loadScript);
@@ -28,7 +30,7 @@ function loadScript() {
     let queue = myObject.queue.length;
     //data serving
     let serving = myObject.serving.length;
-    let ChartCanva = document.getElementById("myChart");
+    let ChartCanva = document.getElementById("queueChart");
     //creating a chart with chart.js
     let myChart = new Chart(ChartCanva, {
         type: 'doughnut',
@@ -72,12 +74,98 @@ function loadScript() {
 
     });
 
-    //06 Number of people served
+    //06 Beer Storage Section
+    let BeerStorage = document.querySelector(".storage-temp").content;
+
+    //Creating Array to use the data
+    storageArray = [];
+    nameArray = [];
+
+    myObject.storage.forEach((ns) => {
+        storageArray.push(ns.amount);
+        // console.log(ns.amount);
+        nameArray.push(ns.name);
+        // console.log(ns.name);
+        let StorageCanvas = document.getElementById("BeerStorageChart").getContext("2d");
+        let GradientBg = StorageCanvas.createLinearGradient(300, 0, 100, 0);
+        GradientBg.addColorStop(0, "#28a745");
+        GradientBg.addColorStop(1, "#ff6a00");
+
+        let StorageChart = new Chart(StorageCanvas, {
+            type: 'horizontalBar',
+            data: {
+                labels: nameArray,
+                datasets: [{
+                    label: "Beer Storage",
+                    data: storageArray,
+                    borderColor: GradientBg,
+                    pointHoverBorderWidth: 1,
+                    pointRadius: 3,
+                    borderWidth: 5,
+                    backgroundColor: GradientBg,
+                }]
+            },
+            options: {
+                animation: false,
+                scales: {
+
+                    xAxes: [{
+                        ticks: {
+                            autoSkip: false,
+                            beginAtZero: true,
+                            fontColor: '#333',
+                            fontSize: 14
+                        },
+                        display: true,
+                        responsive: true,
+
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            fontColor: '#333',
+                            fontSize: 14,
+                            // mirror: true
+
+                        },
+                        display: true,
+                        responsive: true,
+
+                    }]
+                },
+                legend: {
+                    labels: {
+                        fontColor: '#333',
+                        fontFamily: 'Helvetica'
+                    }
+
+                }
+            },
+
+        });
+
+
+    });
+
+
+
+
+
+
+
+
+
 
     //07 Getting a number of beers served in total
 
 
 };
+
+
+
+
+
+
 
 
 //02 Bartenders name-status-status detail
